3e9c2c81c827d7b7a3c9018b71a9e9cb0397db30
Aleksey <61455269+Lexxon90@users.noreply.github.com>
2020-12-07 14:21:53 +0500

    Add files via upload

--- Unsplash API/src/index.js
+++ Unsplash API/src/index.js
@@ -1,21 +1,3 @@
 import "./css/style.css";
 
-// Создаем экземпляр объекта для доступа к API
-const unsplash = new Unsplash({
-    // accesskey из настроек вашего приложения
-    accesskey: "UaSusWVA_p5bK-ZG0Aj_KK-F3yYC8ZuXIxKKvj1Q6AA",
-    // Application Secret из настроек вашего приложения
-    secret: "1oWJZ2W4HqnkQ4bikZkeX9f8fALUbzCR2p4Oy7Fy04w",
-    // Полный адрес страницы авторизации приложения (Redirect URI)
-    // Важно: этот адрес обязательно должен быть указан в настройках приложения на сайте Unsplash API/Developers
-   
-    callbackUrl: "http://www.example.com/auth"
-   });
-   // Генерируем адрес страницы аутентификации на unsplash.com
-   // и указываем требуемые разрешения (permissions)
-   const authenticationUrl = unsplash.auth.getAuthenticationUrl([
-    "public",
-    "write_likes"
-   ]);
-   // Отправляем пользователя по этому адресу
-   location.assign(authenticationUrl);
+console.log("Hello");
