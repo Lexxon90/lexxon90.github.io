237eb99f3a911333eca4ddb1f1e4f39248527fab
Aleksey <61455269+Lexxon90@users.noreply.github.com>
2020-12-08 08:38:17 +0500

    Add files via upload

--- Unsplash API/src/index.js
+++ Unsplash API/src/index.js
@@ -4,23 +4,25 @@ import Unsplash from 'unsplash-js';
 // Создаем экземпляр объекта для доступа к API
 const unsplash = new Unsplash({
     // accesskey из настроек вашего приложения
-    accesskey: "UaSusWVA_p5bK-ZG0Aj_KK-F3yYC8ZuXIxKKvj1Q6AA",
+    accessKey: "UaSusWVA_p5bK-ZG0Aj_KK-F3yYC8ZuXIxKKvj1Q6AA",
     // Application Secret из настроек вашего приложения
     secret: "1oWJZ2W4HqnkQ4bikZkeX9f8fALUbzCR2p4Oy7Fy04w",
     // Полный адрес страницы авторизации приложения (Redirect URI)
     // Важно: этот адрес обязательно должен быть указан в настройках приложения
     // на сайте Unsplash API/Developers
-    callbackUrl: "https://lexxon90.github.io/Unsplash%20API/dist/index.html"
+    callbackUrl: "https://lexxon90.github.io/Unsplash%20API/dist/"
    });
 
 // Генерируем адрес страницы аутентификации на unsplash.com
 // и указываем требуемые разрешения (permissions)
+console.log(unsplash.accesskey)
 const authenticationUrl = unsplash.auth.getAuthenticationUrl([
     "public",
     "write_likes"
    ]);
    // Отправляем пользователя по этому адресу
-   location.assign(authenticationUrl);
+//    location.assign(authenticationUrl);
+console.log(authenticationUrl);
 
 // Считываем GET-параметр code из URL
 // www.example.com/auth?code=abcdef123456...
