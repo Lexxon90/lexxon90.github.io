9c6ecdef575400f87ff204833370b726758a1fd9
Lexxon90 <lexxon90@yandex.ru>
2020-12-24 09:45:38 +0500

    Unsplash API on React test1

--- Unsplash API/src/index.js
+++ Unsplash API/src/index.js
@@ -18,7 +18,7 @@ const unsplash = new Unsplash({
     // accesskey из настроек вашего приложения
     accessKey: "UaSusWVA_p5bK-ZG0Aj_KK-F3yYC8ZuXIxKKvj1Q6AA",
     // Application Secret из настроек вашего приложения
-    // secret: "1oWJZ2W4HqnkQ4bikZkeX9f8fALUbzCR2p4Oy7Fy04w",
+    secret: "1oWJZ2W4HqnkQ4bikZkeX9f8fALUbzCR2p4Oy7Fy04w",
     // Полный адрес страницы авторизации приложения (Redirect URI)
     // Важно: этот адрес обязательно должен быть указан в настройках приложения
     // на сайте Unsplash API/Developers
