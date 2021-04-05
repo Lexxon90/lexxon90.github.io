893bf1187ff9b47842e668b3c3124ca50e4ea56f
Lexxon90 <lexxon90@yandex.ru>
2021-04-05 16:28:09 +0500

    finish desctop
    finish desctop

--- HTML-Code/BlanchardDiplom/src/catalog.js
+++ HTML-Code/BlanchardDiplom/src/catalog.js
@@ -10,23 +10,6 @@ fetch("http://localhost:8080/src/catalog.json")
     let catalog = response;
     let languageItemSelected;
 
-    // Определяем кто выделен
-    function changeCard() {
-      let selectMan = document.querySelector('.age-accordion__item-select');
-      let childSelectMan = selectMan.childNodes[0].textContent;
-      let parentSelectMan = selectMan.parentElement.id;
-      languageItemSelected = document.querySelector('.language-list__selected').id;
-      let selectManCatalog = catalog[languageItemSelected][parentSelectMan][childSelectMan];
-
-      let keys = Object.keys(catalog[languageItemSelected][parentSelectMan]);
-      console.log(keys)
-      console.log(keys.indexOf(childSelectMan))
-
-      document.querySelector('.biography-card__img').style.backgroundImage = 'url('+selectManCatalog.img+')';
-      document.querySelector('.biography-card__name').textContent = childSelectMan;
-      document.querySelector('.biography-card__date').textContent = selectManCatalog.date;
-      document.querySelector('.biography-card__text').textContent = selectManCatalog.bio;
-    }
     function addNewList(){
       // Определяем язык
       languageItemSelected = document.querySelector('.language-list__selected');
@@ -63,6 +46,7 @@ fetch("http://localhost:8080/src/catalog.json")
 
          let languageItemSelectedID = document.querySelector('.language-list__selected').id;
          let parentAccordionItemID = this.parentElement.id;
+         accordionHeaderID = parentAccordionItemID;
          let accordionItemTextContent = this.textContent;
          let catalogAccordionItem = catalog[languageItemSelectedID][parentAccordionItemID][accordionItemTextContent]
          document.querySelector('.biography-card__img').style.backgroundImage = 'url('+catalogAccordionItem.img+')';
@@ -72,8 +56,17 @@ fetch("http://localhost:8080/src/catalog.json")
        })
      })
    }
-
+   let header;
+   let keysNumber;
     $( "#accordion" ).on( "accordionactivate", function(event, ui) {
+      
+      if (document.querySelector('.age-accordion__item-select')) {
+        let language = document.querySelector('.language-list__selected').id;
+        let name = document.querySelector('.age-accordion__item-select');
+        header = name.parentElement.id;
+        let keys = Object.keys(catalog[language][header]);
+        keysNumber = keys.indexOf(name.childNodes[0].textContent)-1;
+      }
       
       languageItemSelected = document.querySelector('.language-list__selected');
       // Определяем ID Header'a
@@ -95,50 +88,74 @@ fetch("http://localhost:8080/src/catalog.json")
          li.appendChild(p);
          acoordionUL.appendChild(li);
        }
+
+      if (ui.newHeader[0].getAttribute('aria-controls') == header) {
+        let list = document.querySelectorAll('.age-accordion__item');
+        list[keysNumber].classList.add('age-accordion__item-select');
+      }
+
        changeCardOnClick();
       
     });
 
     // Изменение языка
-
-    let languageItem = document.querySelectorAll('.language-list__item')
+    let accordionHeaderID = 'age-01';
+    let languageItem = document.querySelectorAll('.language-list__item');
     languageItem.forEach(item=>{
       item.addEventListener('click', function(){
 
-        let selectMan = document.querySelector('.age-accordion__item-select');
-        let childSelectMan = selectMan.childNodes[0].textContent;
-        let parentSelectMan = selectMan.parentElement.id;
-        let languageItemSelectedID = document.querySelector('.language-list__selected').id;
-        // let selectManCatalog = catalog[languageItemSelected][parentSelectMan][childSelectMan];
+        // ID Выбранного языка предыдущего
+        let lastLanguageItemSelectedID = document.querySelector('.language-list__selected').id;
 
-        let keys = Object.keys(catalog[languageItemSelectedID][parentSelectMan]);
-        let keysNumber = keys.indexOf(childSelectMan)
-
+        // Смена класса language-list__selected, для выделение картинки с флагом
         for (let i = 0; i < languageItem.length; i++) {
           languageItem[i].classList.remove('language-list__selected')
         }
         this.classList.add('language-list__selected');
+        // Смена языка основной информации в шапке Каталога
         languageItemSelected = document.querySelector('.language-list__selected');
         let catalogInfo = document.querySelector('.catalog__text');
         catalogInfo.textContent = catalog[languageItemSelected.id].info;
         
         // Меняет язык Header аккордиона
         let accordionHeader = document.querySelectorAll('.age-accordion__title');
-        keys = Object.keys(catalog[languageItemSelected.id]);
-
+        let keys = Object.keys(catalog[languageItemSelected.id]);
         for (let i=0; i<accordionHeader.length; i++) {
           accordionHeader[i].textContent = catalog[languageItemSelected.id][keys[i+1]].info
         }
-        addNewList();
-        keys = Object.keys(catalog[languageItemSelected.id][parentSelectMan]);
-        childSelectMan=keys[keysNumber];
-        let selectManCatalog = catalog[languageItemSelected.id][parentSelectMan][childSelectMan];
 
+
+
+        // Имя выделенного деятеля
+        let biographyCardName = document.querySelector('.biography-card__name').textContent;
+        // Создаем массив деятелей из каталога
+        keys = Object.keys(catalog[lastLanguageItemSelectedID][accordionHeaderID]);
+        // Определяем номер позиции в массиве выбранного деятеля
+        let keysNumber = keys.indexOf(biographyCardName)
+        // Формируем список деятелей заданного языка
+        addNewList();
+        // Заменяем карточку на заданный язык
+        keys = Object.keys(catalog[languageItemSelected.id][accordionHeaderID]);
+        biographyCardName=keys[keysNumber];
+        let selectManCatalog = catalog[languageItemSelected.id][accordionHeaderID][biographyCardName];
         document.querySelector('.biography-card__img').style.backgroundImage = 'url('+selectManCatalog.img+')';
-        document.querySelector('.biography-card__name').textContent = childSelectMan;
+        document.querySelector('.biography-card__name').textContent = biographyCardName;
         document.querySelector('.biography-card__date').textContent = selectManCatalog.date;
         document.querySelector('.biography-card__text').textContent = selectManCatalog.bio;
 
+
+        let nameCardMan = document.querySelector('.biography-card__name').textContent;
+        let acoordionUL = document.querySelector('.ui-accordion-header-active').getAttribute('aria-controls');
+        keys = Object.keys(catalog[languageItemSelected.id][acoordionUL]);
+        let keysSearch = keys.indexOf(nameCardMan)-1;
+        let accordionList = document.querySelectorAll('.age-accordion__item');
+        console.log(acoordionUL)
+        console.log(accordionHeaderID)
+
+        if (acoordionUL == accordionHeaderID) {
+          accordionList[keysSearch].classList.add('age-accordion__item-select');
+        }
+        
 
         changeCardOnClick();
       })
