71e2f2c77d6adae048a0d81036821aafe50a51d9
Lexxon90 <lexxon90@yandex.ru>
2021-07-27 15:43:11 +0500

    add select + datapicker

--- test/index.js
+++ test/index.js
@@ -63,11 +63,97 @@ p
         })
     })
 
-fetch("file:///C:/Teterin_work/JS/GitHub/lexxon90.github.io/test/catalog.json")
+fetch("https://lexxon90.github.io/test/catalog.json")
     .then(function (response) {
         return response.json();
     })
     .then(function (response) {
-        let catalog = response;
-        console.log(catalog)
+        let catalog = response.car;
+        console.log(catalog);
+        // Создаем массив Марок Авто
+        arrMark = [];
+        for (let i=0; i<catalog.length; i++){
+            arrMark.push(catalog[i].mark)
+        }
+        // Создаем массив из уникальных наименований Марок Авто
+        let inputMark = Array.from(new Set(arrMark));
+        // Добавляем Марки авто в DOM
+        for (let i=0; i<inputMark.length; i++) {
+            let option = document.createElement("option");
+            option.classList = "option-mark";
+            option.value = inputMark[i];
+            option.textContent = inputMark[i];
+            let select = document.querySelector(".mark");
+            select.appendChild(option);
+        }
+        // Создаем событе на изменение в выпадающем списке
+        document.querySelector('.mark').addEventListener('change', function(){
+            // текущее выбранное значение
+            let value = this.value;
+            // создание массива с выбранной маркой авто
+            let arrModel=catalog.filter(item=>item.mark === value);
+
+            // Создаем массив Моделей Авто
+            let arrModelOnly = [];
+            for (let i=0; i<arrModel.length; i++){
+                arrModelOnly.push(arrModel[i].model)
+            }
+            // Создаем массив из уникальных наименований Моделей Авто
+            let inputModel = Array.from(new Set(arrModelOnly));
+
+            // Очистить поля от прошлых Моделей
+            let model = document.querySelector(".model");
+            while (model.firstChild) {
+                model.removeChild(model.firstChild);
+            }
+            // Добавляем новые записи в Модели Авто
+            for (let i=0; i<inputModel.length; i++) {
+                let option = document.createElement("option");
+                option.classList = "option-model";
+                option.value = inputModel[i];
+                option.textContent = inputModel[i];
+                let select = document.querySelector(".model");
+                select.appendChild(option);
+            }  
+        })
+
+        document.querySelector('.model').addEventListener('change', function(){
+             // текущее выбранное значение
+             let value = this.value;
+             let mark = document.querySelector('.mark').value
+             // создание массива с выбранной маркой авто
+             let arrModel=catalog.filter(item=>{(item.model === value) && (item.mark === mark)});
+             console.log(arrModel)
+            // Создаем массив Года выпуска Авто
+            let arrYearOnly = [];
+            for (let i=0; i<arrModel.length; i++){
+                arrYearOnly.push(arrModel[i].year)
+            }
+            console.log(arrYearOnly)
+            // Создаем массив из уникальных наименований Моделей Авто
+            let inputYear = Array.from(new Set(arrYearOnly));
+
+            // Очистить поля от прошлых Моделей
+            let year = document.querySelector(".year");
+            while (year.firstChild) {
+                year.removeChild(year.firstChild);
+            }
+            // Добавляем новые записи в Модели Авто
+            for (let i=0; i<inputYear.length; i++) {
+                let option = document.createElement("option");
+                option.classList = "option-year";
+                option.value = inputYear[i];
+                option.textContent = inputYear[i];
+                let select = document.querySelector(".year");
+                select.appendChild(option);
+            }
+        })
+
+        $(function(){
+            $("#datepicker").datepicker({
+                minDate: new Date(2021, 2 - 1, 14),
+                maxDate: new Date(2021, 2 - 1, 25)
+            });
+        });
+        
     })
